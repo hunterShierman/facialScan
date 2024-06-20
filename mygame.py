566f334652f37d776a88ed9539193c24efcb60d4
cjskora <cjskora@uwaterloo.ca>
2024-06-20 14:59:30 -0400

    worked on main loop ;]

--- mygame.py
+++ mygame.py
@@ -1,3 +1,44 @@
-#apifnoijwoeij
+import pygame
+pygame.init()
 
-#suck on deez
+
+screen = pygame.display.set_mode((640, 700))
+screen.fill("blue")
+pygame.display.set_caption('SHIP GAME')
+fps = 60
+
+running = True
+plane = pygame.image.load('plane.png')
+plane = pygame.transform.scale(plane, (100, 100))
+background = pygame.image.load('background.jpeg')
+background = pygame.transform.scale(background, (640, 700))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background, (0, 0))
+    screen.blit(plane, (0, 0))
+    
+    pygame.display.flip()
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('plane.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (640 // 2, 650)
+        self.speed = 5
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.right < 700:
+            self.rect.x += self.speed
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
